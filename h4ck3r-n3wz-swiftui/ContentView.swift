93ff0c1943949727d36b1797ba8164614e2cd743
FrancaTM <francatm@outlook.com>
2021-04-02 16:30:21 -0300

    Initial Commit

---
+++ h4ck3r-n3wz-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  h4ck3r-n3wz-swiftui
+//
+//  Created by Tulio Marcos Franca on 02/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
